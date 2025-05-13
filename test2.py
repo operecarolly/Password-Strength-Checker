044af0b2f226b95d4a14727af1f62710055ff14a
carollygit <carolyopere353@gmail.com>
2025-05-13 12:36:54 +0300

    Remove old test files

--- test2.py
+++
@@ -1,6 +0,0 @@
-import functools
-
-letters=["H","E","L","L","O"]
-word=functools.reduce(lambda x,y,:x+y,letters)
-
-print(word)
--- test3.py
+++
@@ -1,5 +0,0 @@
-squares =[]
-for i in range(1,11):
-    squares.append(i*i)
-    
-print(squares)
